c9a4f2ee596c6b80ce79fb69dff0dfab8b07c013
Aníbal Rodríguez <anibalsci@MacBook-Air-de-Anibal.local>
2017-09-19 22:07:52 -0300

    Initial commit

---
+++ profiles_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Library/WebServer/Documents/profiles-rest-api/profiles_api/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
