f35fca3f38155f38e943511403e5c35183ff222e
Aníbal Rodríguez <anibalsci@MacBook-Air-de-Anibal.local>
2017-09-22 12:55:33 -0300

    laberinto1

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-//Se agregó esta libreria
 from PIL import Image
 
 # Create your models here.
